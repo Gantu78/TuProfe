cc55da5b403f0f0f519913331380c3b3406e913f
Jeol28 <jorgeolaya2803@gmail.com>
2026-05-04 00:01:14 -0500

    Modo debug sin errores

--- TuProfe/app/src/main/java/com/example/tuprofe/BaseApplication.kt
+++ TuProfe/app/src/main/java/com/example/tuprofe/BaseApplication.kt
@@ -6,6 +6,7 @@ import android.os.Debug
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.firestore.firestore
 import dagger.hilt.android.HiltAndroidApp
 
@@ -16,6 +17,10 @@ class BaseApplication: Application() {
         FirebaseApp.initializeApp(this)
 
         if (Debug.isDebuggerConnected()) {
+            // Disable offline cache to avoid conflicts when switching back to production
+            Firebase.firestore.firestoreSettings = FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(false)
+                .build()
             Firebase.firestore.useEmulator("10.0.2.2", 8080)
             Firebase.auth.useEmulator("10.0.2.2", 9099)
         }
