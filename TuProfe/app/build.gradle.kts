bdab8439d84d12f06c5f88e8516e0a1e27fc9af1
Jeol28 <jorgeolaya2803@gmail.com>
2026-05-14 20:10:38 -0500

    No se que hice

--- TuProfe/app/build.gradle.kts
+++ TuProfe/app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation("com.google.maps.android:maps-compose:4.4.1")
 
     implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     
     // Coil
     implementation("io.coil-kt:coil-compose:2.7.0")
